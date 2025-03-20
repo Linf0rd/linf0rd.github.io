a5f40980e19c1daaa98d633edb9b851e5f066857
Linf0rd <linfordsnagger@gmx.com>
2025-03-20 12:15:15 +0200

    Implemented a different layout and improved responsiveness.

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navRight = document.querySelector('.nav-right');
+
+hamburger.addEventListener('click', () => {
+  navRight.classList.toggle('show');
+});
